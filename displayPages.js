7dff76ded2f02c07b7a2c483f5d40d0a45e499d7
unknown <arsnyder1011@gmail.com>
2024-03-06 10:27:51 -0600

    Added product pages. No layout yet established

--- displayPages.js
+++ displayPages.js
@@ -160,9 +160,9 @@ function displayItem(item, itemInfo, page) {
     li.id = item.split(" ").join("_");
     li.classList.add("items-container")
     if(itemInfo[item]["price"] != itemInfo[item]["original price"]){
-        li.innerHTML = `<div class="item-image"><img src=${itemInfo[item]["image"]} width=350 height=350 class="item-image"></div><hr><div class="item"><p>${item}: <s> $${itemInfo[item]["original price"]}</s> $${itemInfo[item]["price"]}</p><p>Inventory: ${itemInfo[item]["inventory"]}</p></div>`;
+        li.innerHTML = `<a href="./productPage.html?page=${pageName}&product=${item}"><div class="item-image"><img src="${itemInfo[item]["image"]}" width=350 height=350 class="item-image"></div><hr><div class="item"><p>${item}: <s> $${itemInfo[item]["original price"]}</s> $${itemInfo[item]["price"]}</p><p>Inventory: ${itemInfo[item]["inventory"]}</p></div></a>`;
     } else {
-        li.innerHTML = `<div class="item-image"><img src=${itemInfo[item]["image"]} width=350 height=350 class="item-image"></div><hr><div class="item"><p>${item}: $${itemInfo[item]["price"]}</p><p>Inventory: ${itemInfo[item]["inventory"]}</p></div>`;
+        li.innerHTML = `<a href="./productPage.html?page=${pageName}&product=${item}"><div class="item-image"><img src="${itemInfo[item]["image"]}" width=350 height=350 class="item-image"></div><hr><div class="item"><p>${item}: $${itemInfo[item]["price"]}</p><p>Inventory: ${itemInfo[item]["inventory"]}</p></div></a>`;
     }
     page.appendChild(li);
     displayedItems.push(item);
