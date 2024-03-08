528f4e07be6cc620f063cac1b619888eb847f420
unknown <arsnyder1011@gmail.com>
2024-03-08 16:23:01 -0600

    Product page complete

--- displayProductPage.js
+++ displayProductPage.js
@@ -8,20 +8,45 @@ console.log(item)
 
 
 function displayItem(pages) {
+    let title = document.querySelector("title")
+
+    title.innerHTML = item
 
     page = document.getElementById("page")
 
     itemInfo = pages["pages"][pageName]["items"]
 
-    li = document.createElement("li");
-    li.id = item.split(" ").join("_");
-    li.classList.add("items-container")
-    if(itemInfo[item]["price"] != itemInfo[item]["original price"]){
-        li.innerHTML = `<a href="./productPage.html?page=${pageName}&product=${item}"><div class="item-image"><img src="${itemInfo[item]["image"]}" width=350 height=350 class="item-image"></div><hr><div class="item"><p>${item}: <s> $${itemInfo[item]["original price"]}</s> $${itemInfo[item]["price"]}</p><p>Inventory: ${itemInfo[item]["inventory"]}</p></div></a>`;
-    } else {
-        li.innerHTML = `<a href="./productPage.html?page=${pageName}&product=${item}"><div class="item-image"><img src="${itemInfo[item]["image"]}" width=350 height=350 class="item-image"></div><hr><div class="item"><p>${item}: $${itemInfo[item]["price"]}</p><p>Inventory: ${itemInfo[item]["inventory"]}</p></div></a>`;
+    navbar = document.getElementById("navbar-product");
+    for (let pageName in pages["pages"]){
+        listElement = document.createElement("li")
+        listElement.className = "navbar"
+        listElement.innerHTML = `<a href="${pages["pages"][pageName]["fileName"]}">${pageName}</a>`
+        navbar.appendChild(listElement)
     }
-    page.appendChild(li);
+
+    div = document.createElement("div");
+    div.classList.add("item-page")
+
+    let saleChange = (parseInt(itemInfo[item]["sold this week"]) - parseInt(itemInfo[item]["sold last week"])) / parseInt(itemInfo[item]["sold last week"]) * 100
+
+    let score = parseInt(itemInfo[item]["rating"]);
+    console.log(score);
+
+    let rating = "";
+    for (let i = 0; i < score; i++){
+        rating += "★";
+    }
+
+    for (let i = 0; i < 5 - score; i++){
+        rating += "☆";
+    }
+
+    if(itemInfo[item]["price"] != itemInfo[item]["original price"]){
+        div.innerHTML = `<img src="${itemInfo[item]["image"]}" width=500 height=500><div class="product-view"><h1>${item}</h1><hr><div class="price"><s> $${itemInfo[item]["original price"]}</s> $${itemInfo[item]["price"]}</div><div class="inventory">In Stock: ${itemInfo[item]["inventory"]}</div><hr><div class="popularity">Popularity Increase over the Past Week: ${saleChange}%</div><div class="rating">User Rating: ${rating}</div></div>`;
+    } else {
+        div.innerHTML = `<img src="${itemInfo[item]["image"]}" width=500 height=500><div class="product-view"><h1>${item}</h1><hr><div class="price">$${itemInfo[item]["price"]}</div><div class="inventory">In Stock: ${itemInfo[item]["inventory"]}</div><hr><div class="popularity">Popularity Increase over the Past Week: ${saleChange}%</div><div class="rating">User Rating: ${rating}</div></div>`;
+    }
+    page.appendChild(div);
 }
 
 
